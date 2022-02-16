988aa8cb2780ce1e1d439f693407d91e5df1a3b7
nimetapaydin <nimetapaydin@gmail.com>
2022-02-16 21:04:50 +0300

    ProductPageSteps düzenlendi.

--- src/main/java/PageObjectModel/Product_Page.java
+++ src/main/java/PageObjectModel/Product_Page.java
@@ -1,28 +1,75 @@
 package PageObjectModel;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.Driver;
+
+import java.time.Duration;
+
 public class Product_Page extends AbstractClass{
 
-   /* WebDriver driver;
+
+    WebDriver driver;
 
     public Product_Page(){
         driver= Driver.getDriver();
         PageFactory.initElements(driver,this);
     }
 
-    @FindBy(css = ".navigationBar")
+    @FindBy(className = "desktopOldAutosuggestTheme-input")
+    private WebElement SearchInput;
 
-    private WebElement navigationBar;
-    public void hoverElement(String hoverElementText){
-        WebElement element = navigationBar.findElement(By.xpath("//span[text()=\"" + hoverElementText + "\"]"));
-        hoverFunction(element);
+    public void clickSearchInput(){
+        clickFunction(SearchInput);
     }
 
-    public void clickableElement(String clickableElementtext){
-        WebElement element = navigationBar.findElement(By.xpath("//a[text()=\"" + clickableElementtext + "\"]"));
-        clickableFunction(element);
+    @FindBy(id = "login")
+    private WebElement loginButton;
+
+    public void clickLoginButton(){
+        clickFunction(loginButton);
     }
 
-*/
+    @FindBy(id = "txtUserName")
+    private WebElement usernametextarea;
+
+    public void typeEmail(String email){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement eMailElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("txtUserName")));
+        eMailElement.click();
+        Actions actions = new Actions(driver);
+        actions.moveToElement(eMailElement).sendKeys(email).perform();
+
+        //sendKeysFunction(usernametextarea,"a@gmail.com");
+    }
+
+    @FindBy(id = "txtPassword")
+    private WebElement passwordtextarea;
+
+    public void typePassword(String password){
+        sendKeysFunction(passwordtextarea,password);
+    }
+
+    @FindBy(id = "btnLogin")
+
+    private WebElement btnLoginButton;
+    public void clickbtnLoginButton(){
+        clickFunction(btnLoginButton);
+    }
+
+    @FindBy(id = "btnEmailSelect")
+
+    private WebElement btnEmailSelect;
+    public void clickbtnEmailSelect(){
+        clickFunction(btnEmailSelect);
+    }
 
 
 }
--- src/main/java/stepDefinitions/ProductPageSteps.java
+++ src/main/java/stepDefinitions/ProductPageSteps.java
@@ -1,4 +1,42 @@
 package stepDefinitions;
 
+import PageObjectModel.Product_Page;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import utilities.Driver;
+
+import java.util.concurrent.TimeUnit;
+
 public class ProductPageSteps {
+    Product_Page product_page = new Product_Page();
+    public WebDriver driver;
+
+    @Given("I focus search bar")
+    public void ı_focus_search_bar() {
+
+        driver = Driver.getDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://www.hepsiburada.com/");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        product_page.clickSearchInput();
+
+
+    }
+    @Given("I type {string}")
+    public void ı_type(String string) {
+
+    }
+    @When("I press Enter")
+    public void ı_press_enter() {
+
+    }
+    @Then("I should see {string} products in list and open product")
+    public void ı_should_see_products_in_list_and_open_product(String string) {
+
+    }
+
+
 }
