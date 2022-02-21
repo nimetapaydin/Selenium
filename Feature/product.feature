f647205c654912e3709028333b9251fd75618227
nimetapaydin <nimetapaydin@gmail.com>
2022-02-21 22:40:01 +0300

    ProductPageSteps düzenlendi.

--- Feature/product.feature
+++ Feature/product.feature
@@ -5,7 +5,19 @@ Feature: Hepsiburada Product
     Given I focus search bar
     Given I type "<product>"
     When I press Enter
-    Then I should see "<product>" products in list and open product
+    Then I should see "<product>" products in list
+    Examples:
+      | language | product    |
+      | TR       | buzdolabı |
+      | EN       | freezer    |
+
+  Scenario Outline: After searching, the product is selected from the list.
+    Given that open "https://hepsiburada.com" link with "<language>" language
+    Given I focus search bar
+    Given I type "<product>"
+    Given I press Enter
+    When  Product is selected from the list.
+    Then I should see "<product>" and open product
     Examples:
       | language | product    |
       | TR       | buzdolabı |
--- src/main/java/stepDefinitions/ProductPageSteps.java
+++ src/main/java/stepDefinitions/ProductPageSteps.java
@@ -14,7 +14,7 @@ public class ProductPageSteps {
     public WebDriver driver;
 
     @Given("I focus search bar")
-    public void ı_focus_search_bar() {
+    public void i_focus_search_bar() {
 
         driver = Driver.getDriver();
         driver.manage().window().maximize();
@@ -25,16 +25,27 @@ public class ProductPageSteps {
     }
 
     @Given("I type {string}")
-    public void ı_type(String product) {
+    public void i_type(String product) {
         product_page.typeProduct(product);
     }
     @When("I press Enter")
-    public void ı_press_enter() {
+    public void i_press_enter() {
         product_page.clickbtnSearchButton();
     }
-    @Then("I should see {string} products in list and open product")
-    public void ı_should_see_products_in_list_and_open_product(String string) {
+
+    @Then("I should see {string} products in list")
+    public void iShouldSeeProductsInList(String arg0) {
         product_page.visibleproductName();
+    }
+
+    @When("Product is selected from the list.")
+    public void productIsSelectedFromTheList() {
+
+    }
+
+    @Then("I should see {string} and open product")
+    public void iShouldSeeAndOpenProduct(String arg0) {
+
     }
 
 
